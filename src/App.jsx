5fce8fe192e54b7506a9b576f88d54f94b2113ef
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-17 18:10:57 +0200

    Install react-icons, add host dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- *
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- *
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
+import AuthRequired from "./components/AuthRequired";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
-import HostLayout from "./components/HostLayout";
-import AuthRequired from "./components/AuthRequired";
+import Dashboard from "./pages/Host/Dashboard";
 
 export default function App() {
   return (
@@ -36,7 +22,9 @@ export default function App() {
           <Route path="vans/:id" element={<VanDetail />} />
 
           <Route element={<AuthRequired />}>
-            <Route path="host" element={<HostLayout />}></Route>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+            </Route>
           </Route>
 
           <Route path="*" element={<NotFound />} />
