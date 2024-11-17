2ac75bb16514b33f9aa0580584fd5953738f1b44
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-17 15:02:51 +0200

    Setup and test db calls

--- api.js
+++ api.js
@@ -7,7 +7,6 @@ import {
   getDoc,
   query,
   where,
-  documentId,
 } from "firebase/firestore/lite";
 
 const firebaseConfig = {
@@ -23,6 +22,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Collection references
 const vansCollectionRef = collection(db, "vans");
-const docRef = doc(db, "vans", id);
+
+export async function getVans() {
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+  return {
+    ...snapshot.data(),
+    id: snapshot.id,
+  };
+}
+
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,29 @@
  * Challenge:
  * Bootstrap the VanLife project by creating the first 2 routes:
  * Home and About.
- * 
+ *
  * Also include the navbar that can link between the two routes.
  * For now, you'll either need to copy/paste the navbar code
  * to both Home and About pages, or you'll need to find a place
  * to put it where it can be shared between the two pages.
  * (Don't overthink this part - just do whatever is easiest for
  * you because we'll learn a better approach very soon)
- * 
+ *
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
 
-function App() {
+import { getVans } from "../api";
 
-  return (
-    <h1>Start here</h1>
-  )
+export default function App() {
+  async function loadVans() {
+    try {
+      const data = await getVans();
+      console.log(data);
+    } catch (err) {
+      console.log(`Error:${err}`);
+    }
+  }
+  loadVans();
+  return <h1>Start here</h1>;
 }
-
-export default App
