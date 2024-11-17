fedd4f85861ff5523a82a3046d45a727b3e74574
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-17 15:37:01 +0200

    Add home page

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,14 @@
  */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
