ad9cb43334b8608d13d1b91b07280ad0b2497dfb
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-17 16:01:44 +0200

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -27,6 +28,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
